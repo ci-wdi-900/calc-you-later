d242160298e040b1a5edf33a646368c4e82f47f3
Colin Jaffe <balloonasaurus@gmail.com>
2020-10-22 13:12:01 -0400

    Pre-lowercases the operation parameter.

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -7,11 +7,12 @@
 function calculate(num1, num2, operation) {
   num1 = Number(num1);
   num2 = Number(num2);
+  operation = operation.toLowerCase();
 
   //The variables are not at all necessary, but they do make the code more readable.
   const isAddition = operation === '+' || operation === 'plus' || operation === 'added to';
   const isSubtraction = operation === '-' || operation === 'minus';
-  const isMultiplication = operation.toLowerCase() === 'x' || operation === 'times' || operation === 'multiplied by';
+  const isMultiplication = operation === 'x' || operation === 'times' || operation === 'multiplied by';
   const isDivision = operation === '/' || operation === 'divided by';
   const isModulus = operation === '%' || operation === 'modulus' || operation === 'mod';
 
@@ -33,19 +34,19 @@ function calculate(num1, num2, operation) {
   }
 
   // Alternate no-intermediate-variables version.
-  if (operation === '+' || operation === 'plus' || operation === 'added to') {
-    return num1 + num2;
-  } else if (operation === '-' || operation === 'minus') {
-    return num1 - num2;
-  } else if (operation.toLowerCase() === 'x' || operation === 'times' || operation === 'multiplied by') {
-    return num1 * num2;
-  } else if (operation === '/' || operation === 'divided by') {
-    return num1 / num2;
-  } else if (operation === '%' || operation === 'modulus' || operation === 'mod') {
-    return num1 % num2;
-  } else {
-    return `Sorry, that's not a mathematical operation!`
-  }
+  // if (operation === '+' || operation === 'plus' || operation === 'added to') {
+  //   return num1 + num2;
+  // } else if (operation === '-' || operation === 'minus') {
+  //   return num1 - num2;
+  // } else if (operation === 'x' || operation === 'times' || operation === 'multiplied by') {
+  //   return num1 * num2;
+  // } else if (operation === '/' || operation === 'divided by') {
+  //   return num1 / num2;
+  // } else if (operation === '%' || operation === 'modulus' || operation === 'mod') {
+  //   return num1 % num2;
+  // } else {
+  //   return `Sorry, that's not a mathematical operation!`
+  // }
 
   /* Or, instead of either of the if/else chains above, we can use
    * a switch, preferably with fall-through to save code.
@@ -53,31 +54,30 @@ function calculate(num1, num2, operation) {
    * saving our boolean checks into variables or having long code.
  */
 
-  // switch(operation.toLowerCase()) {
+  // switch(operation) {
   //   case 'added to':
   //   case 'plus':
   //   case '+':
   //     return num1 + num2;
-  // 
+  
   //   case '-':
   //   case 'minus':
-  //   case 'subtracted from':
   //     return num1 - num2;
-  // 
+  
   //   case 'x':
   //   case 'times':
   //   case 'multiplied by':
   //     return num1 * num2;
-  // 
+  
   //   case '/':
   //   case 'divided by':
   //     return num1 / num2;
-  // 
+  
   //   case '%':
   //   case 'mod':
   //   case 'modulus':
   //     return num1 % num2;
-  // 
+  
   //   default:
   //     return `Sorry, that's not a mathematical operation!`;
   // }
